5f49eb675d985a495d3e54681d3c509ac13f9d54
Adam Wu <963645233@qq.com>
2022-11-13 02:43:42 -0500

    added gorountines for publishing in publish_cameras.go, still testing

--- publish_cameras.go
+++ publish_cameras.go
@@ -28,7 +28,7 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func main() {
+func fetchFromDB() []*Camera {
 	// Set client options
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
@@ -77,12 +77,6 @@ func main() {
 	// Close the cursor once finished
 	cur.Close(context.TODO())
 
-	// fmt.Printf("Found multiple documents (array of pointers): %+v\n", results)
-	// fmt.Println(len(results))
-	// fmt.Printf("type of results: %T \n", results)
-	// fmt.Println(*results[0])
-	// fmt.Println(results[0].ID)
-
 	// Close the connection once no longer needed
 	err = client.Disconnect(context.TODO())
 
@@ -91,7 +85,56 @@ func main() {
 	} else {
 		fmt.Println("Connection to MongoDB closed.")
 	}
+	return results
+}
 
+func pub(start int, end int, results []*Camera) {
+	// fmt.Println("test")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"traffic_topic", // name
+		"topic",         // type
+		true,            // durable
+		false,           // auto-deleted
+		false,           // internal
+		false,           // no-wait
+		nil,             // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	// fmt.Println("test")
+	// fmt.Println(start, end)
+	for i := start; i < end; i++ {
+		// fmt.Println(i)
+		concatenated := fmt.Sprintf("%s %v %v", results[i].ID, results[i].Latitude, results[i].Longitude)
+		// fmt.Println(concatenated)
+		body := concatenated
+		// body := bodyFromDB(os.Args)
+		err = ch.PublishWithContext(ctx,
+			"traffic_topic", // exchange
+			"cameras",       // routing key
+			false,           // mandatory
+			false,           // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+
+		log.Printf(" [x] Sent %s", body)
+	}
+}
+
+func main() {
 	// RabbitMQ
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
@@ -115,25 +158,37 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	//concatenated := strings.Join([]string{results[0].ID, strconv.FormatFloat(float64(results[0].Latitude)), strconv.FormatFloat(float64(results[0].Latitude))}, " ")
-	for i := 0; i < len(results); i++ {
-		concatenated := fmt.Sprintf("%s %v %v", results[i].ID, results[i].Latitude, results[i].Longitude)
-		fmt.Println(concatenated)
-		body := concatenated
-		// body := bodyFromDB(os.Args)
-		err = ch.PublishWithContext(ctx,
-			"traffic_topic", // exchange
-			"cameras",       // routing key
-			false,           // mandatory
-			false,           // immediate
-			amqp.Publishing{
-				ContentType: "text/plain",
-				Body:        []byte(body),
-			})
-		failOnError(err, "Failed to publish a message")
+	var results []*Camera = fetchFromDB()
 
-		log.Printf(" [x] Sent %s", body)
+	//pub(0, len(results), results, ch, ctx, err)
+	//pub(0, len(results)/2, results)
+
+	pub := func(start int, end int) {
+		for i := start; i < end; i++ {
+			concatenated := fmt.Sprintf("%s %v %v", results[i].ID, results[i].Latitude, results[i].Longitude)
+			fmt.Println(concatenated)
+			body := concatenated
+			// body := bodyFromDB(os.Args)
+			err = ch.PublishWithContext(ctx,
+				"traffic_topic", // exchange
+				"cameras",       // routing key
+				false,           // mandatory
+				false,           // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(body),
+				})
+			failOnError(err, "Failed to publish a message")
+			log.Printf(" [x] Sent num %d message -- %s", i, body)
+		}
 	}
+
+	go pub(0, len(results)/2)
+	go pub(len(results)/2, len(results))
+
+	time.Sleep(10 * time.Second)
+	// fmt.Println("----------\n")
+	// pub(0, 10)
 }
 
 func bodyFromDB(args []string) string {
