46964fa8daa755db6f22830ab1e14fe253d2f050
Adam Wu <963645233@qq.com>
2022-11-14 01:06:11 -0500

    fixed case-insensitivity for schema, added image url with multithreadingbase64 encoding, added last update and last modified

--- get-cameras.go
+++ get-cameras.go
@@ -1,34 +1,38 @@
 // reference: https://tutorialedge.net/golang/consuming-restful-api-with-go/
 // https://www.mongodb.com/blog/post/mongodb-go-driver-tutorial
+// https://freshman.tech/snippets/go/image-to-base64/
 package main
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Response struct {
-	ID        			string  `bson:"ID"`
-	Latitude  			float32 `bson:"Latitude"`
-	Longitude 			float32 `bson:"Longitude"`
-	Url       			string  `bson:"Url"`
-	CityName  			string  `bson:"CityName"`
-    Organization 		string `bson:"Organization"`
-    RoadwayName 		string `bson:"RoadwayName"`
-    DirectionOfTravel 	string `bson:"DirectionOfTravel"`
-    Name 				string `bson:"Name"`
-    Status 				string `bson:"Status"`
-    Description 		string `bson:"Description"`
-    Image 				string `bson:"Image"`
-    LastUpdate	 		string `bson:"LastUpdate"`
+	ID                string  `bson:"ID"`
+	Organization      string  `bson:"Organization"`
+	RoadwayName       string  `bson:"RoadwayName"`
+	DirectionOfTravel string  `bson:"DirectionOfTravel"`
+	Latitude          float32 `bson:"Latitude"`
+	Longitude         float32 `bson:"Longitude"`
+	Name              string  `bson:"Name"`
+	Url               string  `bson:"Url"`
+	Status            string  `bson:"Status"`
+	Description       string  `bson:"Description"`
+	CityName          string  `bson:"CityName"`
+	Image             string  `bson:"Image"`
+	LastUpdate        string  `bson:"LastUpdate"`
+	LastModified      string  `bson:"LastModified"`
 }
 
 func main() {
@@ -37,18 +41,49 @@ func main() {
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
-	} 
+	}
 
 	responseData, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-    //fmt.Println(string(responseData))
-	// fmt.Printf("type of response: %T\n", responseData)
 
 	responseObjects := make([]Response, 0, 0)
 	json.Unmarshal(responseData, &responseObjects)
-	// fmt.Println(len(responseObjects))
+
+	// base64 encode image from url, add multiple goroutines to speed up
+	to_base64 := func(start int, end int) {
+		for i := start; i < end; i++ {
+			rs, err := http.Get(responseObjects[i].Url)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer rs.Body.Close()
+
+			bytes, err := ioutil.ReadAll(rs.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			responseObjects[i].Image = base64.StdEncoding.EncodeToString(bytes)
+			// fmt.Println(responseObjects[i].Image)
+		}
+	}
+
+	go to_base64(0, len(responseObjects)/4)
+	go to_base64(len(responseObjects)/4, len(responseObjects)/2)
+	go to_base64(len(responseObjects)/2, len(responseObjects)/4*3)
+	go to_base64(len(responseObjects)/4*3, len(responseObjects))
+
+	time.Sleep(100 * time.Second)
+
+	fmt.Println("finished base64 encoding for images")
+
+	for i := 0; i < len(responseObjects); i++ {
+		responseObjects[i].LastUpdate = time.Now().Format(time.RFC3339)
+		responseObjects[i].LastModified = time.Now().Format(time.RFC3339)
+	}
 
 	// save to mongodb
 	// Set client options
@@ -69,15 +104,28 @@ func main() {
 	fmt.Println("Connected to MongoDB!")
 
 	// Get a handle for your collection
-	collection := client.Database("traffic").Collection("cameras")
+	collection_history := client.Database("cvst_pubsub").Collection("camera")
+
+	client.Database("cvst_pubsub").Collection("cameraCurrent").Drop(context.TODO())
+
+	collection_realtime := client.Database("cvst_pubsub").Collection("cameraCurrent")
 
 	for i := 0; i < len(responseObjects); i++ {
 		// Insert a single document
-		insertResult, err := collection.InsertOne(context.TODO(), responseObjects[i])
+		insertResult, err := collection_history.InsertOne(context.TODO(), responseObjects[i])
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+		fmt.Println("Inserted a single document to history collection: ", insertResult.InsertedID)
+	}
+
+	for i := 0; i < len(responseObjects); i++ {
+		// Insert a single document
+		insertResult, err := collection_realtime.InsertOne(context.TODO(), responseObjects[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Inserted a single document to realtime collection: ", insertResult.InsertedID)
 	}
 
 	// Close the connection once no longer needed
