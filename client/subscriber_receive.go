e7c2b73f63b0f10b908fa3d1ddbcfe22d3c81b0c
Adam Wu <963645233@qq.com>
2022-11-27 13:23:15 -0500

    addded saving to local function for client program, to run: go run client/subscriber_receive.go <user email> <saving dir>

--- client/subscriber_receive.go
+++ client/subscriber_receive.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -19,9 +20,25 @@ func main() {
 		fmt.Println("we did not receive your name")
 		os.Exit(1)
 	}
-	if len(os.Args) > 2 {
-		fmt.Println("only one user name is allowed")
+	if len(os.Args) > 3 {
+		fmt.Println("only one user name and one saving directory is allowed")
 		os.Exit(2)
+	}
+
+	user_name := os.Args[1]
+
+	saving := false
+	var saving_dir string
+
+	if len(os.Args) > 2 {
+		saving = true
+		saving_dir = os.Args[2]
+
+		_, err := os.Open(saving_dir)
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
 	}
 
 	// connecting to RabbitMQ
@@ -45,12 +62,12 @@ func main() {
 
 	// declare user queue for the current user
 	q, err := ch.QueueDeclare(
-		os.Args[1], // name
-		true,       // durable
-		false,      // autoDelete
-		false,      // exclusive
-		false,      // noWait
-		nil,        // args
+		user_name, // name
+		true,      // durable
+		false,     // autoDelete
+		false,     // exclusive
+		false,     // noWait
+		nil,       // args
 	)
 
 	if err != nil {
@@ -77,6 +94,27 @@ func main() {
 	go func() {
 		for msg := range msgs {
 			log.Printf("Successfully received: %s", msg.Body[:100])
+
+			// save to local by topic
+			if saving {
+				saving_file := filepath.Join(saving_dir, msg.RoutingKey+".txt")
+				f, err := os.OpenFile(saving_file, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0755)
+
+				if err != nil {
+					fmt.Print(err.Error())
+					os.Exit(1)
+				}
+
+				_, err_w := f.Write(msg.Body)
+
+				if err_w != nil {
+					fmt.Print(err_w.Error())
+					os.Exit(1)
+				}
+
+				f.Close()
+			}
+
 		}
 	}()
 
